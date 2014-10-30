c5f88ad804e0523635dafe749ce0ec5c92ef79a6
DrMoriarty <drmoriarty.0@gmail.com>
2014-10-30 16:56:47 +0400

    fix: android plugin

--- src/android/SocialOk.java
+++ src/android/SocialOk.java
@@ -1,17 +1,30 @@
-package ru.trilan.googleplus;
+package ru.trilan.socialok;
 
 import org.json.JSONException;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 
 import android.content.Context;
+import android.widget.Toast;
 import android.util.Log;
+import android.os.AsyncTask;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.IOException;
+
+import ru.ok.android.sdk.Odnoklassniki;
+import ru.ok.android.sdk.OkTokenRequestListener;
+import ru.ok.android.sdk.util.OkScope;
 
 public class SocialOk extends CordovaPlugin {
-  public static final String TAG = "SocialOk";
-  public static final String INIT = "initSocialOk";
+  private static final String TAG = "SocialOk";
+  private static final String ACTION_INIT = "initSocialOk";
+  private static final String ACTION_SHARE = "share";
+  private Odnoklassniki odnoklassnikiObject;
+  private CallbackContext _callbackContext;
 
   /**
    * Gets the application context from cordova's main activity.
@@ -23,11 +36,76 @@ public class SocialOk extends CordovaPlugin {
 	
   @Override
   public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
-    boolean result = false;
-    final String sourceUrl, imageUrl, description;
-    //callbackContext.success();
-    callbackContext.error("Not implemented");
+    this._callbackContext = callbackContext;
+    if(ACTION_INIT.equals(action)) {
+      return init(args.getString(0), args.getString(1), args.getString(2));
+    } else if (ACTION_SHARE.equals(action)) {
+      return shareOrLogin(args.getString(0), args.getString(1));
+    }
+    return true;
+  }
 
-    return result;
+  private boolean init(String appId, String secret, String key)
+  {
+    odnoklassnikiObject = Odnoklassniki.createInstance(webView.getContext(), appId, secret, key);
+    _callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
+    _callbackContext.success();
+    return true;
+  }
+
+  private boolean shareOrLogin(final String url, final String comment)
+  {
+    //определяем callback на операции с получением токена
+    odnoklassnikiObject.setTokenRequestListener(new OkTokenRequestListener() {
+        @Override
+        public void onSuccess(String token) {
+          Log.i(TAG, "Odnoklassniki accessToken = " + token);
+          if (token == null)
+            Toast.makeText(webView.getContext(), "Не удалось авторизоваться в приложении через \"Одноклассников\"."
+                           + "\nОшибка на сервере \"Одноклассников\".", Toast.LENGTH_LONG).show();
+          else
+            share(url, comment);
+        }
+
+        @Override
+        public void onCancel() {
+          Log.i(TAG, "Auth cancel");
+          Toast.makeText(webView.getContext(), "Не удалось авторизоваться в приложении через \"Одноклассников\"."
+                         + "\nПроверьте соединение с Интернетом.", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onError() {
+          Log.i(TAG, "Auth error");
+          Toast.makeText(webView.getContext(), "Ошибка во время авторизации в приложении через \"Одноклассников\".",
+                         Toast.LENGTH_LONG).show();
+        }
+      });
+    //вызываем запрос авторизации. После OAuth будет вызван callback, определенный для объекта
+    odnoklassnikiObject.requestAuthorization(webView.getContext(), false, OkScope.VALUABLE_ACCESS);
+    return true;
+  }
+
+  private boolean share(final String url, final String comment)
+  {
+    final Map<String, String> params = new HashMap<String, String>();
+    params.put("linkUrl", url);
+    params.put("comment", comment);
+    new AsyncTask<String, Void, String>() {
+      @Override protected String doInBackground(String... args) {
+        try {
+          return odnoklassnikiObject.request("share.addLink", params, "get");
+        } catch (IOException e) {
+          e.printStackTrace();
+          _callbackContext.error("Error");
+        }
+        return null;
+      }
+      @Override protected void onPostExecute(String result) {
+        Log.i(TAG, "OK share result" + result);
+        _callbackContext.success();
+      }
+    }.execute();
+    return true;
   }
 }
